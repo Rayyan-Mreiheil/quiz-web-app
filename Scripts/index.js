497c1c52c2f16c3b9d773b64b4b67a2e856f119f
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-22 06:11:00 +0300

    Initialize <> users array in localStorage if not already set

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,11 @@
 
-// Show SignUp Form in index.html
+// Show SignUp Form
 const notMemberLink = document.getElementById("notMemberLink");
 notMemberLink.addEventListener("click", function () {
   document.getElementById("form2-signup").style.display = "block";
 });
+
+// Initialize users array in localStorage if not already set
+if (!localStorage.getItem("users")) {
+    localStorage.setItem("users", JSON.stringify([]));
+  }
