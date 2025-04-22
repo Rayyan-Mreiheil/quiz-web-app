22583edf755bc3e00956544032d92e0433e0b5c2
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-22 06:35:27 +0300

    Find <> current clicked button and its ID

--- Scripts/home.js
+++ Scripts/home.js
@@ -3,3 +3,15 @@
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 const username = currentUser.name;
 document.getElementById("player-name").textContent = username;
+
+const btnStart = document.getElementsByClassName("btnStart");
+
+Array.from(btnStart).forEach((button) => {
+  button.addEventListener("click", function (event) {
+    // find current clicked btn
+    const clickedId = event.target.id; // or use this.id
+    // Store clicked ID in localStorage
+    localStorage.setItem("selectedbtnQu", clickedId);
+    
+  });
+});
