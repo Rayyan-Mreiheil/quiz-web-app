3c3d97ba875781c4a83811abd191215dab0a80bd
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-22 06:12:08 +0300

    Add <> default admin on first load

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,3 @@
-
 // Show SignUp Form
 const notMemberLink = document.getElementById("notMemberLink");
 notMemberLink.addEventListener("click", function () {
@@ -7,5 +6,15 @@ notMemberLink.addEventListener("click", function () {
 
 // Initialize users array in localStorage if not already set
 if (!localStorage.getItem("users")) {
-    localStorage.setItem("users", JSON.stringify([]));
-  }
+  localStorage.setItem("users", JSON.stringify([]));
+}
+
+// Add default admin on first load
+const defaultAdmin = {
+  name: "Admin",
+  email: "admin@quiz.com",
+  password: "admin12345",
+  scores: [],
+};
+
+let users = JSON.parse(localStorage.getItem("users"));
