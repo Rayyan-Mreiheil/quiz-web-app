340562833dba5c8f554e8bcfb66021c101c9b1ba
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-22 07:05:44 +0300

    Get <> the table body element

--- Scripts/dashboard.js
+++ Scripts/dashboard.js
@@ -1,3 +1,6 @@
 
 // Load all registered users from localStorage
 const users = JSON.parse(localStorage.getItem("users")) || [];
+
+// Get the table body element
+const userTableBody = document.getElementById("user-table").getElementsByTagName("tbody")[0];
