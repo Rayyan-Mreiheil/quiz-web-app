b0f9340a76b2182d6706e7cd2bef346bf1ecea19
Rayyan-Mreiheil <rayyanmwork@gmail.com>
2025-04-22 06:09:38 +0300

    Add <> EventListener on button to show signup form

---
+++ Scripts/index.js
@@ -0,0 +1,6 @@
+
+// Show SignUp Form in index.html
+const notMemberLink = document.getElementById("notMemberLink");
+notMemberLink.addEventListener("click", function () {
+  document.getElementById("form2-signup").style.display = "block";
+});
